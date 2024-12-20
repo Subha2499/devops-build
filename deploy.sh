cfc4dd5da06a3cb8465ca94016ed727783e7eddc
Subha2499 <subhapvr@gmail.com>
2024-12-20 17:45:58 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,13 +3,13 @@ if [[ $GIT_BRANCH == "origin/dev" ]]; then
     sh 'chmod +x build.sh'
     sh './build.sh'
     docker login -u subha2499 -p 
-    docker tag test ar8888/dev
-    docker push ar8888/dev
+    docker tag test subha2499/dev
+    docker push subha2499/dev
 
 elif [[ $GIT_BRANCH == "origin/main" ]]; then
     sh 'chmod +x build.sh'
     sh './build.sh'
-    docker login -u ar8888 -p dckr_pat_EADWHoJ3ZHIAazRONGu5WkNt0EY
-    docker tag test ar8888/prod 
-    docker push ar8888/prod
+    docker login -u subha2499 -p dckr_pat_YwYa7pkRfFOxGmt1QVWVtOpCeG0
+    docker tag test subha2499/prod 
+    docker push subha2499/prod
 fi
